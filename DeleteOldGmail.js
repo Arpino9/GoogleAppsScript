97aca92125f6ce9777283e670e16fe8c4f95b89d
アルピーノ <77906739+Arpino9@users.noreply.github.com>
2024-06-06 16:24:09 +0900

    Add files via upload

---
+++ DeleteOldGmail.js
@@ -0,0 +1,9 @@
+function deleteOldGmails() {
+  // 5年以上前のメールを削除
+  var deleteThreads = GmailApp.search('older_than:5y -is:starred -is:important');
+  Logger.log('該当スレッド: ' + deleteThreads.length + '件');
+  for (var i = 0; i < deleteThreads.length; i++) 
+  {
+    deleteThreads[i].moveToTrash();
+  }
+}
