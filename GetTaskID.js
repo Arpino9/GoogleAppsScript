8a3bf85e7ca3d438d2bb82ddf12b0200f84e76e0
アルピーノ <77906739+Arpino9@users.noreply.github.com>
2024-06-06 16:52:31 +0900

    Add files via upload

---
+++ GetTaskID.js
@@ -0,0 +1,43 @@
+/**
+ * 編集用
+ */
+const Questions = [];
+
+// シート名
+const SheetName = "タスク一覧";
+// シートID(シートURLの「https://docs.google.com/spreadsheets/d/」から「/edit#gid=0」までの文字列)
+const SpreadSheetId = "1tc5uFTh09PBVVnV2OYmGZ3svY6C-6SwCAF6KIUO8l9c";
+// カラム数
+const ColumnCount = 2;
+// 出力開始行
+let OutPutStartRow = 2;
+
+/**
+ * タスクリストの取得
+ */
+// シート定義
+let Sheet       = SpreadsheetApp.openById(SpreadSheetId).getSheetByName(SheetName);
+
+function GetTaskLists() {
+  var taskLists = Tasks.Tasklists.list();
+
+  if (!taskLists.items)
+  {
+    Logger.log('No task lists found.');
+  }
+
+  // シートのクリア
+  Sheet.getRange(OutPutStartRow, 1, Sheet.getLastRow() + 1, ColumnCount).clearContent();
+
+  var list = Tasks.Tasklists.list();
+  list.items.forEach(function(item) 
+  {
+    Questions.push([item.title, item.id]);
+    Logger.log(item.title + " : " + item.id);
+  });
+
+  taskLists.items.sort();
+
+  Questions.sort();
+  Sheet.getRange(OutPutStartRow,1, Questions.length, Questions[0].length).setValues(Questions);
+}
